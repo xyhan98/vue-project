beaa51517395ad2ea6f4bd5a3cfbe9e14e8adcc2
xyhan98 <xinyi.han98@outlook.com>
2024-02-12 23:44:47 +0800

    initialize product CategoryView

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 
-import { ref } from 'vue'
-import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue'
+import { ref } from "vue";
+import { Menu as IconMenu, Message, Setting } from "@element-plus/icons-vue";
 
 const item = {
-  date: '2016-05-02',
-  name: 'Tom',
-  address: 'No. 189, Grove St, Los Angeles',
-}
-const tableData = ref(Array.from({ length: 20 }).fill(item))
+  date: "2016-05-02",
+  name: "Tom",
+  address: "No. 189, Grove St, Los Angeles"
+};
+const tableData = ref(Array.from({ length: 20 }).fill(item));
 </script>
 
 <template>
   <el-container class="layout-container">
-    <el-aside width="250px">
+    <el-aside>
       <el-scrollbar>
-        <el-menu :default-openeds="['1', '3']">
+        <el-menu :default-openeds="['1', '3']" router="true">
           <el-sub-menu index="1">
             <template #title>
-              <el-icon><message /></el-icon>Navigator One
+              <el-icon><message /></el-icon>Product
             </template>
             <el-menu-item-group>
               <template #title>Group 1</template>
-              <el-menu-item index="1-1">Option 1</el-menu-item>
+              <el-menu-item index="/product/category">Category</el-menu-item>
               <el-menu-item index="1-2">Option 2</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="Group 2">
@@ -76,9 +76,7 @@ const tableData = ref(Array.from({ length: 20 }).fill(item))
       <el-header style="text-align: right; font-size: 12px">
         <div class="toolbar">
           <el-dropdown>
-            <el-icon style="margin-right: 8px; margin-top: 1px"
-              ><setting
-            /></el-icon>
+            <el-icon style="margin-right: 8px; margin-top: 1px"><setting /></el-icon>
             <template #dropdown>
               <el-dropdown-menu>
                 <el-dropdown-item>View</el-dropdown-item>
@@ -93,11 +91,12 @@ const tableData = ref(Array.from({ length: 20 }).fill(item))
 
       <el-main>
         <el-scrollbar>
-          <el-table :data="tableData">
+          <!-- <el-table :data="tableData">
             <el-table-column prop="date" label="Date" width="140" />
             <el-table-column prop="name" label="Name" width="120" />
             <el-table-column prop="address" label="Address" />
-          </el-table>
+          </el-table> -->
+          <RouterView />
         </el-scrollbar>
       </el-main>
     </el-container>
@@ -105,6 +104,9 @@ const tableData = ref(Array.from({ length: 20 }).fill(item))
 </template>
 
 <style scoped>
+.layout-container {
+  height: 700px;
+}
 .layout-container .el-header {
   position: relative;
   background-color: var(--el-color-primary-light-7);
@@ -113,12 +115,13 @@ const tableData = ref(Array.from({ length: 20 }).fill(item))
 .layout-container .el-aside {
   color: var(--el-text-color-primary);
   background: var(--el-color-primary-light-8);
+  width: 250px;
 }
 .layout-container .el-menu {
   border-right: none;
 }
 .layout-container .el-main {
-  padding: 0;
+  padding: 20px;
 }
 .layout-container .toolbar {
   display: inline-flex;
