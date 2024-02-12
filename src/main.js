303722d6a355720103afe0271e5d108596014ac9
xyhan98 <xinyi.han98@outlook.com>
2024-02-12 20:02:42 +0800

    format code and add element-plus

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+// import ElementPlus from "element-plus";
+// import "element-plus/dist/index.css";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+// app.use(ElementPlus);
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
